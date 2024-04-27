da862e120a462c9c5cb73240a41ad8805cc9f94b
kwjung111 <kwkwjung@gmail.com>
2024-04-27 20:24:52 +0900

    ts-jest, prettier 추가

--- ts/server.ts
+++ ts/server.ts
@@ -1,20 +1,21 @@
-import 'reflect-metadata'
-import express from 'express'
-import config from './config'
-import logger from './loaders/logger'
-
+import 'reflect-metadata';
+import express from 'express';
+import config from './config';
+import logger from './loaders/logger';
 
 async function start() {
-    const server = express();
+  const server = express();
 
-    await require('./loaders').default({ expressApp: server });
+  await require('./loaders').default({ expressApp: server });
 
-    server.listen(config.port, () => {
-        logger.info(`${config.env} server listening at ${config.port}!`)
-    }).on('error', err => {
-        logger.error(err);
-        process.exit(1)
+  server
+    .listen(config.port, () => {
+      logger.info(`${config.env} server listening at ${config.port}!`);
     })
+    .on('error', err => {
+      logger.error(err);
+      process.exit(1);
+    });
 }
 
 start();
